069559fb60648c2853424bac61577d39078b11fb
SBOAAAA <fun_pu@hotmail.com>
2022-11-23 17:28:30 +0700

    edit

---
+++ spec/fifaball2022-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import Fifaball2022View from '../lib/fifaball2022-view';
+
+describe('Fifaball2022View', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
